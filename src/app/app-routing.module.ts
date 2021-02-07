34da7c873bb427184307492777a021f89a83f1fe
hp <rmaru470@gmail.com>
2021-02-07 14:14:38 +0530

    initial

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { DefaultComponent } from './layouts/default/default.component';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { PostsComponent } from './modules/posts/posts.component';
+import { TeachersComponent } from './modules/teachers/teachers.component';
+
+const routes: Routes = [{
+  path: '',
+  component: DefaultComponent,
+  children: [{
+    path: '',
+    component: DashboardComponent
+  }, {
+    path: 'posts',
+    component: PostsComponent
+  }, {
+  path: 'teachers',
+  component: TeachersComponent
+}]
+}];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
